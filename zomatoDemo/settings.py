8eca35b8311a8764b4457d10c01862dd5878145f
deepakpeethambaran99 <62935603+deepakpeethambaran99@users.noreply.github.com>
2022-03-03 13:06:09 +0530

    Update settings.py

--- zomatoDemo/settings.py
+++ zomatoDemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+4bhf)s19j9rzsya+*lprp^r1_d^4v=oh%(^)&x79r36k&!f##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
